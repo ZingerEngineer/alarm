1cc195ebf2050f988a3b2e820b014255578b5d81
ZingerEngineer <ramyisez@gmail.com>
2021-10-05 15:47:39 +0200

    alarm progress

---
+++ script.js
@@ -0,0 +1,13 @@
+let hours = document.querySelector(".slot-hours")
+let minutes = document.querySelector(".slot-minutes")
+let seconds = document.querySelector(".slot-seconds")
+const hoursArray = [1,2,3,4,5,6,7,8,9,10,11,12]
+const minutesArray = []
+for (let i = 1 ;i < 60 ; i++) {
+    minutesArray.push(i)
+}
+const secondsArray = []
+for (let i = 1 ;i < 60 ; i++) {
+    secondsArray.push(i)
+}
+
